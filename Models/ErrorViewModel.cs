2c7363fee7d922b1ed9a02507e6766a97ee223ce
OakeseskAo <oakes.a@live.com>
2023-03-15 22:45:44 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuizPrepAi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
