8763c4a83db41c1209250cd7c5f19f479174d73f
OakeseskAo <oakes.a@live.com>
2023-04-10 21:40:39 -0400

    fixed api retreival
    fixed api retreival

--- Models/Settings/AppSettings.cs
+++ Models/Settings/AppSettings.cs
@@ -5,5 +5,25 @@
         public string OpenAiAPIKey { get; set; }
         public QuizPrepAiSettings QuizPrepAiSettings { get; set; }
         public OpenAiSettings OpenAiSettings { get; set; }
+        public AppSettings(IConfiguration configuration)
+        {
+            // Check if the OpenAI API key environment variable is set
+            var openAiApiKey = Environment.GetEnvironmentVariable("OpenAiAPIKey");
+
+            if (!string.IsNullOrEmpty(openAiApiKey))
+            {
+                // Use the environment variable value if it is set
+                OpenAiAPIKey = openAiApiKey;
+            }
+            else
+            {
+                // Otherwise, use the value from the secrets.json file
+                OpenAiAPIKey = configuration.GetValue<string>("OpenAiAPIKey");
+            }
+
+            // Initialize other settings properties
+            QuizPrepAiSettings = configuration.GetSection("QuizPrepAiSettings").Get<QuizPrepAiSettings>();
+            OpenAiSettings = configuration.GetSection("OpenAiSettings").Get<OpenAiSettings>();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -41,18 +41,18 @@ var scope = app.Services.CreateScope();
 await DataHelper.ManageDataAsync(scope.ServiceProvider);
 
 
-app.UseMigrationsEndPoint();
+
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseMigrationsEndPoint();
-//}
-//else
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
